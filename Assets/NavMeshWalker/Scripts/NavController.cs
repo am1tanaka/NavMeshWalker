69ddba58a506b04ad3725aaece5aff253827d927
Yu Tanaka <YRK00337@nifty.com>
2018-08-10 23:39:25 +0900

    近くの再移動時に回転してしまう症状を解消

--- Assets/NavMeshWalker/Scripts/NavController.cs
+++ Assets/NavMeshWalker/Scripts/NavController.cs
@@ -32,29 +32,35 @@ namespace AM1.Nav
         float turnAngle = 45f;
         [TooltipAttribute("ターン時の旋回速度"), SerializeField]
         float turnAngularSpeed = 1000f;
+        [TooltipAttribute("スピードを落とす距離。目的地がこの距離以内になったら、旋回角度に応じた減速をする"), SerializeField]
+        float speedDownDistance = 1f;
+        [TooltipAttribute("停止距離。この距離以下は移動しない"), SerializeField]
+        float stopDistance = 0.01f;
 
         [Header("アニメーション")]
         [TooltipAttribute("移動速度とアニメーション速度の変換率"), SerializeField]
         float Speed2Anim = 1f;
-        [TooltipAttribute("停止とみなす速度"), SerializeField]
+        [TooltipAttribute("アニメを停止とみなす速度"), SerializeField]
         float stopSpeed = 0.01f;
-        [TooltipAttribute("停止距離。この距離以下は移動しない"), SerializeField]
-        float stopDistance = 0.01f;
+        [TooltipAttribute("アニメの平均化係数"), SerializeField]
+        float averageSpeed = 0.5f;
 
         NavMeshAgent agent;
         Animator anim;
         CharacterController chrController;
-        Vector3 velocity;
         Vector3 destination;
+        /// <summary>
+        /// アニメ速度を少し慣らすための値
+        /// </summary>
+        float lastSpeed;
 
         public bool IsReached
         {
             get
             {
                 Vector3 dest = destination;
-                dest.y = 0f;
                 Vector3 pos = transform.position;
-                pos.y = 0f;
+                pos.y = dest.y = 0f;
                 float dist = Vector3.Distance(destination, transform.position);
                 return dist <= stopDistance;
             }
@@ -70,6 +76,8 @@ namespace AM1.Nav
             agent.speed = 0f;
             agent.angularSpeed = 0f;
             agent.acceleration = 0f;
+
+            SetDestination(transform.position);
         }
 
         /// <summary>
@@ -106,56 +114,75 @@ namespace AM1.Nav
                         break;
                     }
                 }
+                Vector3 dest = destination;
+                dest.y = transform.position.y;
 
                 // 移動方向と速度を算出
                 move = target - transform.position;
                 move.y = 0f;
-                spd = Mathf.Min(spd, move.magnitude);
+                float rot = angularSpeed * Time.deltaTime;
 
-                //　移動距離が停止距離より遠い場合、角度と移動設定
-                if (move.magnitude >= stopDistance)
+                //　移動距離が目的地までの距離より遠い場合、角度と移動設定
+                if (Vector3.Distance(dest, transform.position) >= stopDistance)
                 {
                     float angle = Vector3.SignedAngle(transform.forward, move, Vector3.up);
-                    float rot = angularSpeed * Time.deltaTime;
+
+                    // 角度がturnAngleを越えていたら速度0
                     if (Mathf.Abs(angle) > turnAngle)
                     {
                         // 最高速度を越えているのでターンのみ
                         rot = turnAngularSpeed * Time.deltaTime;
                         rot = Mathf.Min(Mathf.Abs(angle), rot);
-                        move = Vector3.zero;
                         transform.Rotate(0f, rot * Mathf.Sign(angle), 0f);
+                        move = Vector3.zero;
+                        spd = 0f;
                     }
                     else
                     {
-                        // 移動しながらターン
-                        rot = Mathf.Min(Mathf.Abs(angle), rot);
-                        transform.Rotate(0f, rot * Mathf.Sign(angle), 0f);
+                        // ターンはしない
+
+                        // ゴール距離がスピードダウンより近い場合、角度の違いの分、前進速度を比例減速する
+                        if (Vector3.Distance(dest, transform.position) < speedDownDistance)
+                        {
+                            spd *= (1f - (Mathf.Abs(angle) / turnAngle));
+                        }
+
+                        // 1回分の移動をキャンセルする場合、回転速度は制限しない
+                        if (move.magnitude < spd)
+                        {
+                            spd = move.magnitude;
+                            rot = angle;
+                            transform.Rotate(0f, angle, 0f);
+                        }
+                        else
+                        {
+                            // 移動しながらターン
+                            rot = Mathf.Min(Mathf.Abs(angle), rot);
+                            transform.Rotate(0f, rot * Mathf.Sign(angle), 0f);
+                        }
+
                         // キャラクターの前方に移動
                         move = transform.forward * spd;
                     }
                 }
+                else
+                {
+                    spd = 0f;
+                    move.Set(0, 0, 0);
+                }
             }
 
-            // 重力加速
-            velocity += Physics.gravity;
-            move.y = velocity.y * Time.deltaTime;
-
-            CollisionFlags flags = chrController.Move(move);
-            if ((flags & CollisionFlags.Below) != 0)
-            {
-                // 地面と接触しているので、Y速度をリセット
-                move.y = 0f;
-            }
-            velocity = move / Time.deltaTime;
+            chrController.Move(move);
             spd = spd / Time.deltaTime;
 
             // アニメーション
             if (anim != null)
             {
-                anim.SetFloat("Speed", spd);
+                lastSpeed = averageSpeed * spd + lastSpeed * (1f - averageSpeed);
+                anim.SetFloat("Speed", lastSpeed);
                 if (spd >= stopSpeed)
                 {
-                    anim.speed = spd * Speed2Anim;
+                    anim.speed = lastSpeed * Speed2Anim;
                 }
                 else
                 {
